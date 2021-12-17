eeb42147a31fcb7ad0052ce384f2ae7571b37163
Julia Thomas <juliathomas@outlook.com>
2021-12-17 15:45:16 +0000

    amended books conteoller, model, views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,24 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all.sort_by(&:name)
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path, notice: 'Book added'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :author, :recommended_by, :complete)
   end
 end
