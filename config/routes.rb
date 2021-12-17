f3018234d7a59aef5de10331fc552b2f5cc35922
Julia Thomas <juliathomas@outlook.com>
2021-12-17 12:37:41 +0000

    added route for new action with new html file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'home/index'
   root 'home#index'
 
-  resources :books, only: [:index, :create, :update, :destroy]
-  resources :films, only: [:index, :create, :update, :destroy]
-  resources :tv_shows, only: [:index, :create, :update, :destroy]
+  resources :books, only: [:index, :new, :create, :update, :destroy]
+  resources :films, only: [:index, :new, :create, :update, :destroy]
+  resources :tv_shows, only: [:index, :new, :create, :update, :destroy]
 end
